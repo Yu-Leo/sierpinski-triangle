e2ad0c9484efc9bfec31591b62ca2ad004a07383
Lev Yuvensky <Levayu22@gmail.com>
2021-10-03 21:14:51 +0300

    Refactoring

--- sierpinski-triangle/main.cpp
+++ sierpinski-triangle/main.cpp
@@ -9,23 +9,32 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
-const int N = 1000;
+const int MAXIMUM_NUMBER_OF_POINTS = 1000;
+
+std::vector<Point> generate_3_vertexes() {
+    std::vector<Point> vertexes;
+    for (int i = 0; i < 3; i++) {
+        vertexes.push_back(Point(rand() % WINDOW_WIDTH, rand() % WINDOW_HEIGHT));
+    }
+    return vertexes;
+}
+
+void draw_points_from_list(sf::RenderWindow& window, const std::vector<Point>& points_list) {
+    for (Point p : points_list) {
+        p.draw(window);
+    }
+}
 
 int main() {
-    
     std::srand(0);
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Sierpinsky triangle");
     window.clear(sf::Color::White);
     window.display();
 
-    std::vector<Point> vertexes;
-    for (int i = 0; i < 3; i++) {
-        vertexes.push_back(Point(rand() % WINDOW_WIDTH, rand() % WINDOW_HEIGHT));
-    }
-
+    std::vector<Point> vertexes = generate_3_vertexes();
     std::vector<Point> points_list;
 
-    int k = 0;
+    int number_of_points = 0;
     
     while (window.isOpen()) {
         sf::Event event;
@@ -33,24 +42,19 @@ int main() {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        if (k < N) {
+
+        if (number_of_points < MAXIMUM_NUMBER_OF_POINTS) {
             points_list.push_back(Point(rand() % WINDOW_WIDTH, rand() % WINDOW_HEIGHT));
-            k++;
+            number_of_points++;
         }
 
         window.clear(sf::Color::White);
-
-        for (Point p : vertexes) {
-            p.draw(window);
-        }
-
-        for (Point p : points_list) {
-            p.draw(window);
-        }
+        draw_points_from_list(window, vertexes);
+        draw_points_from_list(window, points_list);
 
         window.display();
 
-        sf::sleep(sf::milliseconds(80));
+        sf::sleep(sf::milliseconds(10));
     }
     return 0;
 }
