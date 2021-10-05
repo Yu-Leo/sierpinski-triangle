848cb3401dcd6d63422243c79566f91714531f44
Lev Yuvensky <Levayu22@gmail.com>
2021-10-05 22:24:02 +0300

    Add generation vertexes only in three zones

--- sierpinski-triangle/main.cpp
+++ sierpinski-triangle/main.cpp
@@ -16,10 +16,31 @@ const int MAXIMUM_NUMBER_OF_POINTS = 10000;
 
 std::vector<Point> generate_3_vertexes() {
     std::vector<Point> vertexes;
-    for (int i = 0; i < 3; i++) {
-        Point p(rand() % WINDOW_WIDTH, rand() % WINDOW_HEIGHT, Point::Type::VERTEX);
-        vertexes.push_back(p);
-    }
+    /*
+        Field:
+        --------------------
+        |   |    1    |    |
+        |   |         |    |
+        --------------------
+        |    2   |    3    |
+        |        |         |
+        --------------------
+        Points generates in this three zones.
+    */
+    
+    Point p1(WINDOW_WIDTH / 4 + rand() % (WINDOW_WIDTH / 2),
+        rand() % (WINDOW_HEIGHT / 2), Point::Type::VERTEX); 
+
+    Point p2(rand() % (WINDOW_WIDTH / 2),
+        (WINDOW_HEIGHT / 2) + rand() % (WINDOW_HEIGHT / 2), Point::Type::VERTEX);
+
+    Point p3(WINDOW_WIDTH / 2 + rand() % (WINDOW_WIDTH / 2),
+        (WINDOW_HEIGHT / 2) + rand() % (WINDOW_HEIGHT / 2), Point::Type::VERTEX);
+
+    vertexes.push_back(p1);
+    vertexes.push_back(p2);
+    vertexes.push_back(p3);
+
     return vertexes;
 }
 
@@ -27,6 +48,36 @@ void draw_points_from_list(sf::RenderWindow& window, const std::vector<Point>& p
     for (Point p : points_list) {
         p.draw(window);
     }
+}
+
+void draw_zone_lines(sf::RenderWindow& window) {
+    // Draw test lines for visualization zones.
+    sf::Color color = sf::Color::Green;
+    int thickness = 2;
+
+    sf::RectangleShape line1(sf::Vector2f(WINDOW_WIDTH, thickness));
+    line1.setPosition(0, WINDOW_HEIGHT / 2);
+    line1.setFillColor(color);
+    window.draw(line1);
+
+    sf::RectangleShape line2(sf::Vector2f(WINDOW_HEIGHT / 2, thickness));
+    line2.rotate(90);
+    line2.setPosition(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+    line2.setFillColor(color);
+    window.draw(line2);
+
+    sf::RectangleShape line3(sf::Vector2f(WINDOW_HEIGHT / 2, thickness));
+    line3.rotate(90);
+    line3.setPosition(WINDOW_WIDTH / 4, 0);
+    line3.setFillColor(color);
+    window.draw(line3);
+
+    sf::RectangleShape line4(sf::Vector2f(WINDOW_HEIGHT / 2, thickness));
+    line4.rotate(90);
+    line4.setPosition(3 * WINDOW_WIDTH / 4, 0);
+    line4.setFillColor(color);
+    window.draw(line4);
+
 }
 
 int main() {
@@ -66,6 +117,8 @@ int main() {
 
         window.clear(sf::Color::White);
        
+        draw_zone_lines(window);
+
         draw_points_from_list(window, points_list);
         draw_points_from_list(window, vertexes);
 
